3661a73384edf74f5adca5f902b75a29b0d2497d
SharvariJoshi99 <sharvari.joshi0799@gmail.com>
2024-07-11 20:52:05 +0530

    Added Live Location of Patient to details view

--- src/app/templates/dashboard/views/user-details/user-details.page.ts
+++ src/app/templates/dashboard/views/user-details/user-details.page.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import Feature from 'ol/Feature';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import { defaults as defaultControls, Attribution } from 'ol/control';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.page.html',
   styleUrls: ['./user-details.page.scss'],
 })
-export class UserDetailsPage implements OnInit {
-
+export class UserDetailsPage implements OnInit, AfterViewInit {
   patient: any;
   colors = ['#7676ab', '#059ab4', '#f03d81', '#064167', '#7b8b49', '#0aade4', '#008d99', '#925fb1', '#445269']
   backgroundColor = '#7676ab';
+  map!: Map;
 
   constructor(private route: Router) {
     const navigation = this.route.getCurrentNavigation();
@@ -23,9 +36,52 @@ export class UserDetailsPage implements OnInit {
     this.setBackgroundColor();
   }
 
+  ngAfterViewInit(): void {
+    const coordinates = [7.999, 4.827]; //getRandomCoordinates();
+    const transformedCoordinates = fromLonLat(coordinates);
+    const marker = new Feature({
+      geometry: new Point(transformedCoordinates),
+    });
+
+    const vectorSource = new VectorSource({
+      features: [marker],
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
+        image: new Icon({
+          src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+          anchor: [0.5, 1],
+        }),
+      }),
+    });
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        vectorLayer,
+      ],
+      view: new View({
+        center: transformedCoordinates,
+        zoom: 14,
+      }),
+    });
+
+  }
+
   setBackgroundColor() {
     const randomIndex = Math.floor(Math.random() * this.colors.length);
     this.backgroundColor = this.colors[randomIndex];
+  }
+
+  getRandomCoordinates(): [number, number] {
+    const longitude = (Math.random() * 360) - 180;
+    const latitude = (Math.random() * 180) - 90;
+    return [longitude, latitude];
   }
 
 
