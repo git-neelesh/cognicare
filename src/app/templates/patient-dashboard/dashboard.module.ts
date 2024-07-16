a3e7a7bf15d85b7e9a5e039bf5cda174361a3049
Akash Garg <akashgarg@Akashs-MacBook-Pro.local>
2024-07-16 22:31:46 +0530

    Onboarding pages added

--- src/app/templates/patient-dashboard/dashboard.module.ts
+++ src/app/templates/patient-dashboard/dashboard.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -32,5 +32,6 @@ import { ChatbotComponent } from 'src/app/chatbot/chatbot.component';
     BottomTabBarComponent,
     ChatbotComponent
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DashboardPageModule {}
