ff95f8e6efc6d53c631e42aa3c037ad22cd2a596
git-neelesh <uno.neelesh@gmail.com>
2024-07-17 13:45:35 +0530

    Landing page card updated

--- src/app/templates/dashboard/models/demo-list.ts
+++ src/app/templates/dashboard/models/demo-list.ts
@@ -8,24 +8,24 @@ export interface Demo {
 
 export const demoList: Demo[] = [
   {
-    title: 'Animations in SwiftUI',
-    subtitle: 'Build and animate an iOS app from scratch',
+    title: 'Your family',
+    subtitle: 'You caring loved ones ',
     caption: '20 sections - 3 hours',
     color: '120, 80, 240',
     image: 'assets/course_rive/topic_1.svg',
   },
   {
-    title: 'Build Quick Apps with SwiftUI',
+    title: 'Caregivers',
     subtitle:
-      'Apply your Swift and SwiftUI knowledge by building real, quick and various applications from scratch',
+      'They will assists you with daily tasks and activities',
     caption: '47 sections - 11 hours',
     color: '103, 146, 255',
     image: 'assets/course_rive/topic_2.svg',
   },
   {
-    title: 'Build a SwiftUI app for iOS 15',
+    title: 'Music you love',
     subtitle:
-      'Design and code a SwiftUI 3 app with custom layouts, animations and gestures using Xcode 13, SF Symbols 3, Canvas, Concurrency, Searchable and a whole lot more',
+      'Rhythm and harmony find their way into the inward places of the Soul.',
     caption: '21 sections - 4 hours',
     color: '0, 95, 231',
     image: 'assets/course_rive/topic_1.svg',
