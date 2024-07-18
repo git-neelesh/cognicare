3e9b5da94e2ded265cf2725c946b7c9bae914416
SharvariJoshi99 <sharvari.joshi0799@gmail.com>
2024-07-18 14:07:40 +0530

    live-location button

--- src/app/templates/dashboard/views/user-details/user-details.page.ts
+++ src/app/templates/dashboard/views/user-details/user-details.page.ts
@@ -10,6 +10,9 @@ import { Router } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
 import { MapService } from 'src/app/templates/services/map.service';
 import { Geolocation } from '@capacitor/geolocation';
+import { HttpClient } from '@angular/common/http';
+import { ChartData, ChartOptions } from 'chart.js';
+
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.page.html',
@@ -17,6 +20,8 @@ import { Geolocation } from '@capacitor/geolocation';
 })
 export class UserDetailsPage implements OnInit, AfterViewInit {
   patient: any;
+  colors = ['#7676ab', '#059ab4', '#f03d81', '#064167', '#7b8b49', '#0aade4', '#008d99', '#925fb1', '#445269']
+  backgroundColor = '#7676ab';
   @ViewChild('map', { static: true }) mapElementRef!: ElementRef;
   map: any;
   source = { lat: 18.5285, lng: 73.8744 };
@@ -25,12 +30,27 @@ export class UserDetailsPage implements OnInit, AfterViewInit {
   directionsDisplay: any;
   sourceMarker: any;
   destination_marker: any;
+  private dataUrl = 'assets/data.json';
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels= [];
+  public barChartType: string = 'bar';
+  public barChartLegend = true;
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Data' }
+    ]
+  };
 
   constructor(
     private route: Router,
     private socket: Socket,
     private mapService: MapService,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private http: HttpClient
   ) {
     const navigation = this.route.getCurrentNavigation();
     if (navigation?.extras?.state) {
@@ -42,6 +62,17 @@ export class UserDetailsPage implements OnInit, AfterViewInit {
   //  this.socket.connect();
    // this.getGeoLocation();
   //  this.getCurrentGeoLocation();
+
+  this.getData().subscribe(data => {
+    console.log(data);
+    //this.barChartLabels = data.map(item => item.name);
+    //this.barChartData.datasets[0].data = data.map(item => item.value);
+  });
+  }
+
+
+  getData(): any {
+    return this.http.get(this.dataUrl);
   }
 
   ngAfterViewInit(): void {
@@ -186,5 +217,9 @@ export class UserDetailsPage implements OnInit, AfterViewInit {
       console.log(data);
       //this.renderMap([data['coords'].latitude, data['coords'].longitude ]);
     });
+  }
+
+  backToDashboard(){
+    this.route.navigate(['/dashboard']);
   }
 }
