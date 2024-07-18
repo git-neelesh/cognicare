e739bc16d90f52c16042e74c35d68a4f30d4f9b4
SharvariJoshi99 <sharvari.joshi0799@gmail.com>
2024-07-18 23:25:42 +0530

    detect identify face

--- src/app/templates/patient-dashboard/views/camera-view/camera-view.component.ts
+++ src/app/templates/patient-dashboard/views/camera-view/camera-view.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { readFileSync } from 'fs-extra';
+import { AzureKeyCredential } from '@azure/core-auth';
+
+import createFaceClient, {
+    getLongRunningPoller,
+    isUnexpected,
+} from '@azure-rest/ai-vision-face';
 
 @Component({
   selector: 'app-camera-view',
@@ -22,7 +28,40 @@ export class CameraViewComponent  implements OnInit {
     });
 
     this.myImage = image.webPath;
+    this.identifyPerson(this.myImage);
     console.log(this.myImage);
   }
 
+  async identifyPerson(myImage) {
+
+    const endpoint = 'https://memorymakers.cognitiveservices.azure.com';
+    const apikey = '62f4c4b15f654617ab2e73a4a94631b3';
+    const credential = new AzureKeyCredential(apikey);
+    const client = createFaceClient(endpoint, credential);
+    const largePersonGroupId = 'family1';
+
+        console.log('Detect faces from the target image.');
+    const detectResponse = await client.path('/detect').post({
+        contentType: 'application/octet-stream',
+        queryParameters: {
+            detectionModel: 'detection_03',
+            recognitionModel: 'recognition_04',
+            returnFaceId: true,
+        },
+        body: readFileSync('demo.jpeg'),
+    });
+    if (isUnexpected(detectResponse)) {
+        throw new Error(detectResponse.body.error.message);
+    }
+    const faceIds = detectResponse.body.map(face => face.faceId as string)
+
+    console.log('Identify the faces in the large person group.');
+    const identifyResponse = await client.path('/identify').post({
+        body: { faceIds, largePersonGroupId },
+    });
+    if (isUnexpected(identifyResponse)) {
+        throw new Error(identifyResponse.body.error.message);
+    }
+    console.log(identifyResponse.body);
+      }
 }
--- src/app/templates/patient-dashboard/views/camera-view/camera-view.module.ts
+++ src/app/templates/patient-dashboard/views/camera-view/camera-view.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CameraViewRoutingModule } from './camera-view-routing.module';
 import { CameraViewComponent } from './camera-view.component';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [CameraViewComponent],
@@ -13,6 +13,7 @@ import { IonicModule } from '@ionic/angular';
     FormsModule,
     CameraViewRoutingModule,
     IonicModule.forRoot(),
+    HttpClientModule,
   ]
 })
 export class CameraViewModule { }
