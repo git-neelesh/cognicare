f71a94cbffcfdaf1721541892c9c157be0baee8d
SharvariJoshi99 <sharvari.joshi0799@gmail.com>
2024-07-18 15:38:08 +0530

    Adding Game Analysis chart

--- src/app/templates/dashboard/views/user-details/user-details.module.ts
+++ src/app/templates/dashboard/views/user-details/user-details.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { UserDetailsPageRoutingModule } from './user-details-routing.module';
-
+import { NgChartsModule } from 'ng2-charts';
 import { UserDetailsPage } from './user-details.page';
 
 @NgModule({
@@ -13,7 +13,7 @@ import { UserDetailsPage } from './user-details.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    UserDetailsPageRoutingModule
+    UserDetailsPageRoutingModule, NgChartsModule
   ],
   declarations: [UserDetailsPage]
 })
