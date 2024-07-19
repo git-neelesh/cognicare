61ab76e6bc375dbdaa703e9716cb2e210058d166
Neelesh Vishwakarma <uno.neelesh@gmail.com>
2024-07-19 07:52:46 +0530

    UI fixes neel (#42)
    * new chat api
    
    * Style and Css fixes
    
    * --amend

--- src/app/templates/dashboard/models/tabs.ts
+++ src/app/templates/dashboard/models/tabs.ts
@@ -9,7 +9,7 @@ export interface BottomTabItem {
 
 export const tabItemsList: BottomTabItem[] = [
   {
-    id: 'tab_chat',
+    id: 'tab_home',
     stateMachine: 'HOME_Interactivity',
     artboard: 'HOME',
     status: false,
@@ -18,8 +18,8 @@ export const tabItemsList: BottomTabItem[] = [
 
   },
   {
-    id: 'tab_search',
-    stateMachine: 'SEARCH_Interactivity',
+    id: 'tab_chat',
+    stateMachine: 'CHAT_Interactivity',
     artboard: 'CHAT',
     status: false,
     show: false,
@@ -27,30 +27,12 @@ export const tabItemsList: BottomTabItem[] = [
 
   },
   {
-    id: 'tab_timer',
-    stateMachine: 'TIMER_Interactivity',
-    artboard: 'TIMER',
-    status: false,
-    show: false,
-    svg:'time-outline'
-
-  },
-  {
     id: 'tab_bell',
     stateMachine: 'BELL_Interactivity',
-    artboard: 'BELL',
+    artboard: 'Notifications will be here',
     status: false,
     show: false,
     svg:'notifications-outline'
-
-  },
-  {
-    id: 'tab_user',
-    stateMachine: 'USER_Interactivity',
-    artboard: 'USER',
-    status: false,
-    show: false,
-    svg:'person-outline'
 
   },
   {
